362260e9d2146c8392cb9b49ab3afc63b3276393
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2025-07-14 22:38:46 -0300

    fix: ✅ Um ponto central para configuração global ✅ Provider disponível em toda a aplicação ✅ Layouts de grupo simples sem duplicação ✅ Manutenção facilitada e código mais limpo

--- app/(layoutMaster)/layout.tsx
+++ app/(layoutMaster)/layout.tsx
@@ -1,18 +1,7 @@
-"use client";
-
-import React from "react";
-import { Providers } from "../providers";
-
 export default function LayoutMasterLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className=" antialiased" suppressHydrationWarning>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,35 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"; // Import de CSS global primeiro
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Maravilhosa Pesca",
-  description: "Peixes frescos, frutos do mar e qualidade para sua família!",
-};
-
-export default function RootLayout({
+export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
